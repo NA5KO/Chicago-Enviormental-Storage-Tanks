bcb1eefe38c8a8ee61fb9fc09a0ebdb96ab55880
Amine <ayahya@welyne.com>
2025-05-15 10:46:58 +0100

    fixation

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ capacity_df = pd.read_csv("data/capacity_merged.csv")
 net_df = pd.read_csv("data/net_merged.csv")
 pivot_df = pd.read_csv("data/pivot_merged.csv")
 products_df = pd.read_csv("data/top_products.csv")
-suburb_df = pd.read_csv("data/suburb.csv")
+suburb_df = pd.read_csv("data/suburb.csv", sep="\t", header=None, names=["Suburb", "Count"])
 trend_df = pd.read_csv("data/trend.csv")
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -24,11 +24,16 @@ def net_growth_chart():
     return dcc.Graph(figure=fig)
 
 def tank_type_trend_chart():
-    fig = px.area(pivot_df, x="Year", y=["ABOVEGROUND STORAGE TANK", "UNDERGROUND STORAGE TANK"], title="Trend of Tank Types Over Time")
+    fig = px.area(
+        pivot_df,
+        x="year",
+        y=["ABOVEGROUND STORAGE TANK", "UNDERGROUND STORAGE TANK"],
+        title="Trend of Tank Types Over Time"
+    )
     return dcc.Graph(figure=fig)
 
 def top_products_chart():
-    fig = px.bar(products_df, x="product", y="count", orientation='h', title="Top 10 Products in Storage Tanks")
+    fig = px.bar(products_df, x="count", y="product", orientation='h', title="Top 10 Products in Storage Tanks")
     return dcc.Graph(figure=fig)
 
 def suburb_distribution_chart():
@@ -36,13 +41,12 @@ def suburb_distribution_chart():
     return dcc.Graph(figure=fig)
 
 def rolling_trend_chart():
-    fig = px.line(trend_df, x="Window", y="5-Year Average", title="5-Year Windowed Installation Trend")
+    fig = px.line(trend_df, x="year", y="mov_avg", title="5-Year Windowed Installation Trend")
     return dcc.Graph(figure=fig)
 
 # Layout
 app.layout = dbc.Container([
     html.H1("Chicago Environmental Tank Dashboard", className="text-center mt-4 mb-4"),
-    
     dbc.Tabs([
         dbc.Tab(label="Capacity Distribution", children=[capacity_chart()]),
         dbc.Tab(label="Net Growth", children=[net_growth_chart()]),
@@ -52,3 +56,6 @@ app.layout = dbc.Container([
         dbc.Tab(label="5-Year Rolling Trend", children=[rolling_trend_chart()]),
     ])
 ], fluid=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
